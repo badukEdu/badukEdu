14d6293b3509af46a6db85eb180bae401ccb41e8
Lee-GiHeung <dlrlgmd25@gmail.com>
2024-02-08 14:49:39 +0900

    entity 추가

--- src/main/java/org/choongang/product/entities/Product.java
+++ src/main/java/org/choongang/product/entities/Product.java
@@ -1,9 +1,6 @@
 package org.choongang.product.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
@@ -12,23 +9,23 @@ public class Product {
     @Id
     @GeneratedValue
     private Long num; // 구매 식별자, 자동 생성되는 고유한 번호 // pk
-    @Column
+    @Column(length = 50, nullable = false)
     private String productName; // 구매상품명
-    @Column
+    @Column(length = 20, nullable = false)
     private String buyerName; // 구매자명
-    @Column
+    @Column(length = 20, nullable = false)
     private String contactNumber; // 연락처
     @Column
     private String orderTotal; // 주문합계
     @Column
     private String paymentMethod; // 결제방법선택
-    @Column
+    @Column(length = 20, nullable = false)
     private String depositorName; // 입금자명
 
     @Column
     private String contentImage; // 콘텐츠 이미지
     @Column
     private String priceSubscriptionPeriod; // 가격/구독기간
-    @Column
+    @Lob
     private String productDescription; // 상품소개
 }
