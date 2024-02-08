7e8b2a2e2365acbbbecb4f4095ca8dd5fb392862
Lee-GiHeung <dlrlgmd25@gmail.com>
2024-02-08 14:44:20 +0900

    entity 추가

--- src/main/java/org/choongang/gameContent/entities/GameContent.java
+++ src/main/java/org/choongang/gameContent/entities/GameContent.java
@@ -1,10 +1,7 @@
 package org.choongang.gameContent.entities;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.Date;
@@ -15,22 +12,22 @@ public class GameContent {
     @Id
     @GeneratedValue
     private Long num; // 게임 식별자, 자동 생성되는 고유한 번호 // pk
-    @Column
+    @Column(length = 50, nullable = false)
     private String gameTitle; // 게임콘텐츠명
-    @Column
+    @Column(nullable = false)
     private int totalGameLevels; // 총게임레벨
-    @Column
+    @Column(nullable = false)
     private int subscriptionMonths; // 구독개월
-    @Column
+    @Column(nullable = false)
     private Date subscriptionPeriod; // 구독가능기간
-    @Column
+    @Column(nullable = false)
     private int originalPrice; // 정가
-    @Column
+    @Column(nullable = false)
     private float discountRate; // 할인율
-    @Column
+    @Column(nullable = false)
     private int salePrice; // 판매가
-    @Column
+    @Lob
     private String packageContents; // 패키지내용
-    @Column
+    @Column(nullable = false)
     private String thumbnail; // 썸네일
 }
