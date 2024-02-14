a76ef54c97fd54d7789de2551142feeb0f14f094
kwi5124 <kwi5124@naver.com>
2024-02-14 20:40:29 +0900

    이메일 인증

--- src/main/java/org/choongang/member/controllers/MemberController.java
+++ src/main/java/org/choongang/member/controllers/MemberController.java
@@ -2,31 +2,34 @@ package org.choongang.member.controllers;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.choongang.commons.ExceptionProcessor;
 import org.choongang.commons.Utils;
+import org.choongang.member.service.FindIdService;
 import org.choongang.member.service.FindPwService;
 import org.choongang.member.service.JoinService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/member")
 @RequiredArgsConstructor
+@SessionAttributes("EmailAuthVerified")
 public class MemberController implements ExceptionProcessor {
 
   private final Utils utils;
   private final JoinService joinService;
-  private final FindPwService findPwService; //비번찾기
+  private final FindPwService findPwService;
+  private final FindIdService findIdService;
+  private final FindIdValidator findIdValidator;
 
   /**
    * 동의 및 인증
@@ -54,16 +57,11 @@ public class MemberController implements ExceptionProcessor {
     return "redirect:/member/join";
   }
 
-  /**
-   * 회원가입
-   * @param form
-   * @param model
-   * @return
-   */
   @GetMapping("/join")
   public String join(@ModelAttribute RequestJoin form, Model model) {
     commonProcess("join", model);
 
+    // 이메일 인증 여부 false로 초기화
     model.addAttribute("EmailAuthVerified", false);
 
     return "front/member/join";
@@ -79,22 +77,83 @@ public class MemberController implements ExceptionProcessor {
       return "front/member/join";
     }
 
+    // EmailAuthVerified 세션값 비우기 */
     sessionStatus.setComplete();
 
     return "redirect:/member/login";
   }
 
-  /**
-   * 로그인
-   * @param model
-   * @return
-   */
   @GetMapping("/login")
   public String login(Model model) {
     commonProcess("login", model);
 
     return "front/member/login";
   }
+
+  @PostMapping("/login")
+  public String loginPs(@Valid RequestJoin form, Errors errors,Model model) {
+    commonProcess("login", model);
+
+    return "redirect:/studyGroup";
+  }
+
+  /* 아이디 찾기 S */
+
+  /**
+   * 아이디 찾기 양식
+   *
+   * @param form
+   * @param model
+   * @return
+   */
+
+  @GetMapping("/find_id")
+  public String findId(@ModelAttribute RequestFindId form, Model model) {
+    commonProcess("find_id", model);
+
+    return "front/member/find_id";
+  }
+
+  /**
+   * 아이디 찾기 처리
+   *
+   * @param form
+   * @param errors
+   * @param model
+   * @return
+   */
+
+  @PostMapping("/find_id")
+  public String findIdPs(@Valid RequestFindId form, Errors errors, Model model) {
+    commonProcess("find_id", model);
+
+    findIdValidator.validate(form, errors);
+
+    if (errors.hasErrors()) {
+
+      return "front/member/find_id";
+    }
+
+    findIdService.sendUserId(form.name(), form.email());
+
+    return "front/member/find_id_done";
+  }
+
+
+  @GetMapping("/find_id_done")
+
+  public String toFindIdDone() {
+
+    return "front/member/find_id_done";
+  }
+
+  @PostMapping("/member/find_id_done")
+  public String findIdDone() {
+
+    return "front/member/find_id_done";
+  }
+
+  /* 아이디 찾기 E */
 
   /**
    * 비밀번호 찾기 양식
@@ -142,23 +201,26 @@ public class MemberController implements ExceptionProcessor {
     return "front/member/find_pw_done";
   }
 
+
   private void commonProcess(String mode, Model model) {
     mode = StringUtils.hasText(mode) ? mode : "join";
     String pageTitle = Utils.getMessage("회원가입", "commons");
 
     List<String> addCss = new ArrayList<>();
     List<String> addScript = new ArrayList<>();
+    List<String> addCommonScript = new ArrayList<>();
 
     if (mode.equals("login")) { // 로그인
       pageTitle = Utils.getMessage("로그인", "commons");
 
-    } else if (mode.equals("confirmation")) { // 회원인증
-      addCss.add("member/confirmation");
-      addScript.add("member/confirmation");
-
     } else if (mode.equals("join")) { // 회원가입
       addCss.add("member/join");
       addScript.add("member/join");
+      addCommonScript.add("address");
+
+    } else if (mode.equals("find_id")) { // 아이디 찾기
+      pageTitle = Utils.getMessage("아이디_찾기", "commons");
+
 
     } else if (mode.equals("find_pw")) { // 비밀번호 찾기
       pageTitle = Utils.getMessage("비밀번호_찾기", "commons");
@@ -167,5 +229,9 @@ public class MemberController implements ExceptionProcessor {
     model.addAttribute("pageTitle", pageTitle);
     model.addAttribute("addCss", addCss);
     model.addAttribute("addScript", addScript);
+    model.addAttribute("addCommonScript", addCommonScript);
+
   }
+
+
 }
