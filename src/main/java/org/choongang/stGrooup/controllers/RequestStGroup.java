0604a0351082924d27a0a8524666d95a247ae531
cbmja <cbmja@naver.com>
2024-02-15 12:45:32 +0900

    스터디그룹

--- src/main/java/org/choongang/stGrooup/controllers/RequestStGroup.java
+++ src/main/java/org/choongang/stGrooup/controllers/RequestStGroup.java
@@ -3,6 +3,7 @@ package org.choongang.stGrooup.controllers;
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -13,17 +14,31 @@ import java.time.LocalDateTime;
 public class RequestStGroup {
 
     private Long num;    //기본키
+
+    @NotBlank
     private String name;     //스터디그룹명
+
+    @NotBlank
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate; //시작일
+
+    @NotBlank
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate; //종료일
+
+    @NotBlank
     private Long maxSubscriber;    //최대인원
+
+    @NotBlank
     private Long maxLevel;   //달성 레벨
     private String text;     //비고
     private String mode = "add";    //수정 , 생성
     private long gameContentNum;    //게임 컨텐츠 번호
+
+    @NotBlank
     private long month;
+
+
     private String gameTitle;
 
 
