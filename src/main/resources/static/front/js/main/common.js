16cf89046262b811ac3f18944a9ae27bc6852c0a
zzangah0529 <zzangah529@gmail.com>
2024-02-21 10:10:23 +0900

    main

---
+++ src/main/resources/static/front/js/main/common.js
@@ -0,0 +1,13 @@
+window.addEventListener("DOMContentLoaded", function(){
+    const swiper = new Swiper(".mySwiper", {
+        loop:true,
+            navigation: {
+              nextEl: ".mySwiper .next",
+              prevEl: ".mySwiper .prev",
+            },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
+
+});
